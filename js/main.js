cc26db83d42a8bd806bc01a61e9c34faaea53b2c
ckelner <chris.kelner@gmail.com>
2015-12-03 02:00:41 -0500

    get awsQuery working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
-// AWS.config.update({accessKeyId: 'akid', secretAccessKey: 'secret'});
-function awsQuery(access, secret) {
-  alert(access);
-  alert(secret);
+function awsQuery() {
+  var ec2 = new AWS.EC2(
+    {
+      accessKeyId: document.getElementById('awsAccessKey').value,
+      secretAccessKey: document.getElementById('awsSecretKey').value,
+      region: 'us-east-1',
+      maxRetries: 5
+    }
+  );
+  var params = {
+    Filters: [
+      { // only find running instances
+        Name: 'instance-state-name',
+        Values: [ 'running' ]
+      },
+    ],
+    MaxResults: 1000 // max
+  };
+  ec2.describeInstances(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data);
+    }
+  });
+  // always return false to avoid page refresh
+  return false;
 }
+
+function init() {
+  document.getElementById("awsQueryButton").addEventListener("click", function(){
+    awsQuery();
+  });
+}
+
+window.onload = function() {
+  init();
+};
